04450ed758fa54d4fb20243d9b449621f6c2aec4
lucacri <lucacri@gmail.com>
2017-09-14 14:42:44 -0400

    Switched to alpine

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,15 @@
-FROM phusion/baseimage
+FROM lucacri/alpine-base:3.6
+MAINTAINER "Luca Critelli" <lucacri@gmail.com>
 
-RUN apt-get update -qqy \
-  && apt-get -qqy --no-install-recommends install \
-    pgbouncer \
-  	&& rm -rf /var/lib/apt/lists/* \
-	&& apt-get autoremove -y \
-	&& apt-get clean \
-	&& apt-get autoclean \
-	&& echo -n > /var/lib/apt/extended_states \
-	&& rm -rf /var/lib/apt/lists/* \
-	&& rm -rf /usr/share/man/?? \
-	&& rm -rf /usr/share/man/??_* 
 
-COPY rootfs /
+RUN apk --no-cache add \
+curl \
+nano \
+bzip2 \
+bash \
+pgbouncer 
+
+ADD rootfs /
 
 ENV PG_ADDRESS_HOST postgres
 ENV PG_ADDRESS_PORT 5432
@@ -22,3 +19,5 @@ ENV PGBOUNCER_MAX_CLIENT_CONN 10000
 ENV PGBOUNCER_POOL_SIZE 400
 ENV PGBOUNCER_IDLE_TIMEOUT 240
 
+ENTRYPOINT ["/init"]
+CMD []
--- rootfs/etc/cont-init.d/89-setup.sh
+++ rootfs/etc/cont-init.d/89-setup.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-set -e
-
-PG_ADDRESS_HOST=${PG_ADDRESS_HOST:-}
-PG_ADDRESS_PORT=${PG_ADDRESS_PORT:-}
-PG_POSTGRES_USER=${PG_POSTGRES_USER:-}
-PG_POSTGRES_PASS=${PG_POSTGRES_PASS:-}
-PGBOUNCER_MAX_CLIENT_CONN=${PGBOUNCER_MAX_CLIENT_CONN:-}
-PGBOUNCER_POOL_SIZE=${PGBOUNCER_POOL_SIZE:-}
-PGBOUNCER_IDLE_TIMEOUT=${PGBOUNCER_IDLE_TIMEOUT:-}
-
+#!/usr/bin/with-contenv bash
 if [ ! -f /etc/pgbouncer/pgbconf.ini ]
 then
 cat << EOF > /etc/pgbouncer/pgbconf.ini
@@ -17,12 +6,12 @@ cat << EOF > /etc/pgbouncer/pgbconf.ini
 * = host=${PG_ADDRESS_HOST} port=${PG_ADDRESS_PORT}
 
 [pgbouncer]
-logfile = /var/log/postgresql/pgbouncer.log
-pidfile = /var/run/postgresql/pgbouncer.pid
+logfile = /var/log/pgbouncer/pgbouncer.log
+pidfile = /var/run/pgbouncer/pgbouncer.pid
 ;listen_addr = *
 listen_addr = 0.0.0.0
 listen_port = 6432
-unix_socket_dir = /var/run/postgresql
+unix_socket_dir = /var/run/pgbouncer
 ;auth_type = any
 auth_type = trust
 auth_file = /etc/pgbouncer/userlist.txt
@@ -39,8 +28,10 @@ if [ ! -s /etc/pgbouncer/userlist.txt ]
 then
         echo '"'"${PG_POSTGRES_USER}"'" "'"${PG_POSTGRES_PASS}"'"'  > /etc/pgbouncer/userlist.txt
 fi
+mkdir /var/run/pgbouncer
+chmod 777 /var/run/pgbouncer
 
 chown -R postgres:postgres /etc/pgbouncer
-chown root:postgres /var/log/postgresql
-chmod 1775 /var/log/postgresql
+chown root:postgres /var/log/pgbouncer
+chmod 1775 /var/log/pgbouncer
 chmod 640 /etc/pgbouncer/userlist.txt
